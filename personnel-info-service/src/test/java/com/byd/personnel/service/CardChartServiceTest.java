a0df2a7d13a75b5d12e674926e5e778252093379
门峰超 <fengchao.men@coolcollege.cn>
2022-10-25 10:24:27 +0800

    删除无用代码

---
+++ personnel-info-service/src/test/java/com/byd/personnel/service/CardChartServiceTest.java
@@ -0,0 +1,16 @@
+package com.byd.personnel.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardChartServiceTest {
+
+    @Test
+    public void queryById() {
+    }
+
+    @Test
+    public void insertCardChart() {
+    }
+}
